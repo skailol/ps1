28cb5bfe93ca52a977111c7956408949902f33ea
skai <127462715+skailol@users.noreply.github.com>
2024-01-11 08:16:55 -0800

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,4 +1,10 @@
-# Open 10 Command Prompt windows
-for ($i = 1; $i -le 10; $i++) {
-    Start-Process "cmd.exe" -ArgumentList "/k echo This is Command Prompt window $i"
-}
+$url = "https://media3.giphy.com/media/unQ3IJU2RG7DO/giphy.gif?cid=ecf05e47javwa978ux022rutbvmkr6z64bc7r52bw3skxzwm&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+$tempFolderPath = $env:TEMP
+$outputPath = Join-Path $tempFolderPath "downloaded.gif"
+
+Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+Write-Host "GIF downloaded to: $outputPath"
+
+# Run the downloaded GIF using the default associated program
+Start-Process $outputPath
