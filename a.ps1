da295e013c1556df572e3120a76ec501a0b392fa
skai <127462715+skailol@users.noreply.github.com>
2024-01-11 08:13:22 -0800

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,12 +1,1 @@
-while ($true) {
-    # Check for closed Command Prompt instances
-    $closedCmd = Get-Process -Name cmd -ErrorAction SilentlyContinue
-
-    # If no Command Prompt found, open a new one
-    if (-not $closedCmd) {
-        Start-Process cmd.exe
-    }
-
-    # Check every second (adjust as needed)
-    Start-Sleep -Seconds 1
-}
+Start-Process cmd.exe
